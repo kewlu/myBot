f14bb05ea1158f1317fa5f7e0e38e11d781d0532
kewlu <pampam@my.com>
2018-10-27 21:10:29 +0300

    cmt

---
+++ MyBot.BLL.Contracts/IQuizService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace MyBot.BLL.Contracts
+{
+    public interface IQuizService
+    {
+        TelegramBotClient _client { get; }
+
+        long _chatId { get; }
+
+        Task CheckMessage(Message _message);
+
+        Task Start();
+    }
+}
