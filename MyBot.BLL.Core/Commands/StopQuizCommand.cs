2224baa645418549e0abd782a3250a8ef0e2e681
kewlu <pampam@my.com>
2018-11-03 00:02:44 +0300

    -add StopQuizCommand(forgotten)

---
+++ MyBot.BLL.Core/Commands/StopQuizCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace MyBot.BLL.Core.Commands
+{
+    public class StopQuizCommand : Command
+    {
+        public override string Name { get => "/StopQuiz" ; set => throw new NotImplementedException(); }
+
+        public override bool Contains(string command)
+        {
+            return (command == this.Name);
+        }
+
+        public override async Task<bool> ExecuteAsync(Message message, TelegramBotClient client)
+        {
+            var _message = message;
+
+
+            if(!BotService.ActiveQuiz.ContainsKey(_message.Chat.Id))
+            {
+                await client.SendTextMessageAsync(_message.Chat.Id, "В этом чате ничего не запущено! Чтобы запустить /Count");
+            }
+            var _quizService = BotService.ActiveQuiz[_message.Chat.Id];
+            if (_quizService.Stop())
+            {
+                BotService.ActiveQuiz.Remove(_message.Chat.Id);
+            }
+            return true;
+
+        }
+    }
+}
